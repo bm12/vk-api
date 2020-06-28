676d389e602cfd740f0365f5382808ea1097a64f
Babaev Mahmoud <mahmud1997babaev@gmail.com>
2020-06-28 22:16:23 +0300

    Further sevices

--- src/services/documentsService.js
+++ src/services/documentsService.js
@@ -3,12 +3,22 @@ import { requestVkApi } from './vkApiBaseService';
 const methodPefix = 'docs.';
 
 export const documnetsService = {
-  search: ({ query }) => {
-    return requestVkApi.get({
-      methodName: `${methodPefix}search`,
-      params: {
-        q: query,
-      },
-    });
-  },
+  search: ({
+    query,
+    count = 20,
+    offset = 0,
+    searchOwn = false,
+    returnTags = false,
+  }) => requestVkApi.get({
+    methodName: `${methodPefix}search`,
+    params: {
+      q: query,
+      search_own: searchOwn,
+      return_tags: returnTags,
+
+      offset,
+      count,
+    },
+  })
+    .then(({ data }) => data.response),
 };
--- src/services/vkApiBaseService.js
+++ src/services/vkApiBaseService.js
@@ -1,19 +1,18 @@
 import axios from 'axios';
 
-const instance = axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://api.vk.com/method/',
 });
 
 const API_VERSION = '5.110';
 
 export const requestVkApi = {
-  get: ({ methodName, params }) => {
-    instance.get(methodName, {
+  get: ({ methodName, params }) => axiosInstance
+    .get(methodName, {
       params: {
         ...params,
-        access_token: '',
+        access_token: process.env.ACCESS_TOKEN,
         v: API_VERSION,
       },
-    });
-  },
+    }),
 };
