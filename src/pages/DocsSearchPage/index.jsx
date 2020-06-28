6bee902e9020d1c60bef4b2ca533571be655ead0
Babaev Mahmoud <mahmud1997babaev@gmail.com>
2020-06-28 22:17:12 +0300

    Add draft request to docs search

--- src/pages/DocsSearchPage/index.jsx
+++ src/pages/DocsSearchPage/index.jsx
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
-import PropTypes from 'prop-types';
+import { documnetsService } from '@/services/documentsService';
 
 import classNames from 'classnames/bind';
 import styles from './styles.scss';
 const cx = classNames.bind(styles);
 
-const DocsSearchPage = () => (
-  <div>
-    <form>
-      <input type="search" />
-      <button type="submit">Search</button>
-    </form>
-    <div className={cx('result-list')}>
+const DocsSearchPage = () => {
+  const [results, setResults] = useState([]);
+  const onSubmit = useCallback(async (e) => {
+    e.preventDefault();
+    const query = e.target.elements['documents-search'].value;
+    const { items } = await documnetsService.search({ query });
 
+    setResults(items);
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="search" name="documents-search" />
+        <button type="submit">Search</button>
+      </form>
+      <div className={cx('results')}>
+        {results.map((result) => (
+          <div className={cx('result')} key={result.id}>
+            {result.type === 4 ? (
+              <img className={cx('result-img')} src={result.url} alt="" />
+            ) : (
+              <div>
+                other type (ext is: {result.ext})
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
-
-DocsSearchPage.defaultProps = {
-
-};
-DocsSearchPage.propTypes = {
-
+  );
 };
 
 export default DocsSearchPage;
